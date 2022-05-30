85bcd76c024f7e2e9a6bdd023223ef0dfda7af71
horsemandotpy <chonmegai@outlook.com>
2022-05-30 12:45:31 +0700

    Complete Porfolio Project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        body: "#17171F",
+        "selected-text": "#4CDC93",
+        theme: "#00CD66",
+        nav: "#404053",
+        secondary: "#9191A4",
+        badge: "#3F3F51",
+        "input-border": "#565666",
+        input: "#2A2A35",
+      },
+      fontFamily: {
+        poppins: ["'Poppins'", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
